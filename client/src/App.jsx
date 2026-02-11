88ab7c6e8fd4c71623beb1e13ec234a2e2be8d96
Remi M <remimaskulin@gmail.com>
2026-02-11 23:32:44 +0200

    feat: gameStart screen, and an ongoing game page with no real functionality aside from some mapping.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GameStart from './gameStart';
+import Game from './Game';
 
 function App() {
   return (
     <>
+      {/* <Header /> */}
+
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<GameStart />} />
+          <Route path="/game" element={<Game />} />
+          {/* <Route path="/user" element={<UserPanel />} /> */}
+        </Routes>
+      {/* <Footer /> */}
     </>
   )
 }
