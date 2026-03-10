76fb9318326182e8083fc06ca8775df3d25e3e3c
Leo Leerilä <Leo.Leerila@metropolia.fi>
2026-03-10 15:33:04 +0200

    update testing

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,15 @@
-require('dotenv').config();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const config = require('../utils/config');
+const logger = require('../utils/logger');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Connected to database`);
+  try {
+    const conn = await mongoose.connect(config.MONGO_URI);
+    logger.info(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    logger.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
- 
+
 module.exports = connectDB;
